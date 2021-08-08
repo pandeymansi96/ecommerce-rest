dc6e5d64d0a59ce8463d732bfaf7d81a5775465e
Mansi Pandey <pandeymansi96@gmail.com>
2021-08-08 17:48:52 +0530

    Completed buyers and sellers operation

--- ecommerce-rest/app/Http/Controllers/BuyersController.php
+++ ecommerce-rest/app/Http/Controllers/BuyersController.php
@@ -2,25 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Buyer;
 use Illuminate\Http\Request;
 
 class BuyersController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        $buyers = Buyer::has('transactions')->get();
+        return response()->json(['count' => $buyers->count(), 'data' => $buyers], 200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function create()
     {
         //
@@ -37,15 +30,11 @@ class BuyersController extends Controller
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function show($id)
     {
-        //
+        $buyers = Buyer::has('transactions')->findOrFail($id);
+        return response()->json(['data' => $buyer], 200);
     }
 
     /**
--- ecommerce-rest/app/Http/Controllers/UsersController.php
+++ ecommerce-rest/app/Http/Controllers/UsersController.php
@@ -13,7 +13,8 @@ class UsersController extends Controller
      */
     public function index()
     {
-        //
+        $users = User::all();
+        return response()->json(['data' => $users], 200);
     }
 
     /**
@@ -34,51 +35,83 @@ class UsersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $rules = [
+            'name' => 'required|min:5',
+            'email' => 'required|unique:users',
+            'password' => 'required|min:8|confirmed'
+        ];
+
+        $this->validate($request, $rules);
+
+        $data = $request->all();
+        $data['password'] = bcrypt($request->password);
+        $data['verified'] = User::UNVERIFIED_USER;
+        $data['verification_token'] = User::generateVerificationCode();
+        $data['admin'] = User::REGULAR_USER;
+
+        $user = User::create($data);
+
+        return response()->json(['data' => $user], 201);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    
+    public function show(User $user)
     {
-        //
+        return response()->json(['data' => $user], 200);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    
+    public function update(Request $request, User $user)
     {
-        //
+        $rules = [
+            'name' => 'min:5',
+            'email' => 'email|unique:users',
+            'password' => 'min:8|confirmed',
+            'admin' => 'in:'.User::REGULAR_USER . ', '. User::ADMIN_USER
+        ];
+
+        $this->validate($request, $rules);
+
+        if($request->has('name')){
+            $user->name = $request->name;
+        }
+
+        if($request->has('email')){
+            $user->verified = User::UNVERIFIED_USER;
+            $user->verification_token = User::generateVerificationCode();
+            $user->email = $request->email;
+        }
+
+        if($request->has('password')){
+            $user->password = bcrypt($request->password);
+        }
+
+        if($request->has('admin')){
+            if(! $user->isVerified()){
+                return response()->json(['error'=> 'Only verified users can become admin','code'=> 409], 409);
+            }
+            $user->admin = $request->admin;
+        }
+
+        if(! $user->isDirty()){
+            return response()->json(['error'=> 'You need to justify a different value to update', 'code' => 422], 422);
+        }
+
+        $user->save();
+
+        return response()->json(['data' => $user], 200);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+    
+    public function destroy(User $user)
     {
-        //
+        $user->delete();
+        return response()->json(['data'=> $user], 200);
     }
 }
